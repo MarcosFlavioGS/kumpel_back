5d2d63885b6733159c3a60e81a315dc7bb6ead73
Marcos Flavio <mfghost69@gmail.com>
2025-02-04 14:51:07 -0300

    feat: add documentation with ex_doc

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule KumpelBack.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:ex_doc, "~> 0.22.0", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:elixir_sense, "~> 1.0.0"},
       {:phoenix, "~> 1.7.14"},
