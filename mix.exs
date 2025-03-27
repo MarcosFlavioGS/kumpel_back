2b5a295a9ddc81e2a21b2ead9f4e97876b2a6da7
Marcos Flavio <mfghost69@gmail.com>
2025-03-26 23:13:36 -0300

    chore: imcrement version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KumpelBack.MixProject do
   def project do
     [
       app: :kumpel_back,
-      version: "0.5.0",
+      version: "0.8.0",
       elixir: "~> 1.17.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
