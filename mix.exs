c4b652f8f02d6922210395400477a1d32202f775
Marcos Flavio <mfghost69@gmail.com>
2025-04-18 18:07:54 -0300

    docs: update version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KumpelBack.MixProject do
   def project do
     [
       app: :kumpel_back,
-      version: "0.11.0",
+      version: "1.0.0",
       elixir: "~> 1.17.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
