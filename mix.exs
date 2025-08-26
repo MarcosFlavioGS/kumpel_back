eb91fec12c672e18548e2bd916c94245963086ae
Marcos Flavio <mfghost69@gmail.com>
2025-08-26 01:20:33 -0300

    build: updating both elixir and credo

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule KumpelBack.MixProject do
     [
       app: :kumpel_back,
       version: "1.0.0",
-      elixir: "~> 1.17.3",
+      elixir: "~> 1.18.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
