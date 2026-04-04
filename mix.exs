38f0f5986ded344d3e4c073f443c759049e89017
Marcos Flavio <mfghost69@gmail.com>
2026-04-03 21:04:59 -0300

    docs: add hex_docs to docs/ directory

--- mix.exs
+++ mix.exs
@@ -6,6 +6,9 @@ defmodule KumpelBack.MixProject do
       app: :kumpel_back,
       version: "1.1.0",
       elixir: "~> 1.18.4",
+      docs: [
+        output: "docs"
+      ],
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
